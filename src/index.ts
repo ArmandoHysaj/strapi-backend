f64037300974c8b35c721047e50f8ed511e1fc1a
ArmandoHysaj <a.hysaj@ext.efg.gg>
2025-02-28 16:29:14 +0100

    latest changes BE

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,50 @@
-// import type { Core } from '@strapi/strapi';
+"use strict";
 
-export default {
+module.exports = {
   /**
    * An asynchronous register function that runs before
    * your application is initialized.
    *
    * This gives you an opportunity to extend code.
    */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+  register(/*{ strapi }*/) {
+    // Getting all the users permissions routes
+    const userRoutes =
+      strapi.plugins["users-permissions"].routes["content-api"].routes;
+    // Set the UUID for our middleware
+    const isUserOwnerMiddleware = "global::user-find-many";
+    const isUserCanUpdateMiddleware = "global::user-can-update";
+
+    // Find the route where we want to inject the middleware
+    const findUser = userRoutes.findIndex(
+      (route) => route.handler === "user.find" && route.method === "GET"
+    );
+
+    // Find the route where we want to inject the middleware
+    const updateUser = userRoutes.findIndex(
+      (route) => route.handler === "user.update" && route.method === "PUT"
+    );
+
+    // helper function that will add the required keys and set them accordingly
+    function initializeRoute(routes, index) {
+      routes[index].config.middlewares = routes[index].config.middlewares || [];
+      routes[index].config.policies = routes[index].config.policies || [];
+    }
+
+    // Check if we found the find one route if so push our middleware on to that route
+    if (findUser) {
+      initializeRoute(userRoutes, findUser);
+      userRoutes[findUser].config.middlewares.push(isUserOwnerMiddleware);
+    }
+
+    // Check if we found the find one route if so push our middleware on to that route
+    if (updateUser) {
+      initializeRoute(userRoutes, updateUser);
+      userRoutes[updateUser].config.middlewares.push(isUserCanUpdateMiddleware);
+    }
+
+    console.log(userRoutes[findUser]);
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +53,5 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  bootstrap(/*{ strapi }*/) {},
 };
