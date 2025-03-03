c42ef696e08061222f412ce36b08b5219518cfe7
ArmandoHysaj <a.hysaj@ext.efg.gg>
2025-03-03 10:07:45 +0100

    Fix: Use env for JWT_SECRET

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default ({
+  env,
+}: {
+  env: (key: string, defaultValue?: string) => string;
+}) => ({
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET", "fallback-secret-key"),
+    },
+  },
+});
